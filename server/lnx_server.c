ce544f7b1a50eda0dfa7e47f0f903ced6f771034
jonpol01 <johnpaul@soliva.me>
2016-06-27 09:40:22 +0900

    0627

--- server/lnx_server.c
+++ server/lnx_server.c
@@ -14,22 +14,37 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
+#include "GenericTypeDefs.h"		//Generic Type Definitions
 
-#define DATA_SERVER_PORT 50000
-#define TRAFFIC_SERVER_PORT 50010
+#define MAIN_SERVER_PORT	50000	//Our Main server Tcp Port
+#define TRAFFIC_SERVER_PORT 50010	//Traffic handler Tcp port
+#define RELAY_SERVER_PORT	50020	//Relay server Tcp Port
+#define CH_LOGGED		1
+#define CH_REQUEST		2
+#define CH_CONTROL		3
 
-static int st_list[255];
-static int st_i = 0;
-static int st_count = 0;
+PRIVATE UINT st_list[255];			//Socket list array
+PRIVATE UINT st_i = 0;				//Socket list count
+PRIVATE UINT st_count = 0;
+BYTE c_Data[256];					//Client to Child data buffer
+size_t size_buff;					//size of data buffer
 
-void dostuff(int);      /* function prototype */
-void dostuff_cmd(int);  /* function Cmd */
-void dostuff_traffic(int);  /* function Update Client count */
+void dostuff(int);			//function prototype
+void dostuff_cmd(int);		//function Cmd
+void dostuff_traffic(int);	//function Update Client count
+void dostuff_Relay(int);	//function
+
+/******** error() *********************
+Exit when error occurs.
+ *****************************************/
 void error(const char *msg){
-	perror(msg);
-	exit(1);
+	perror(msg);		//do process error
+	exit(1);			//exit
 }
 
+/******** printarray() *********************
+Print array for debugging.
+ *****************************************/
 void printarray (int arg[], int length) {
 	int n;
 	for (n=0; n<length; ++n){
@@ -38,52 +53,56 @@ void printarray (int arg[], int length) {
 	printf("\n");
 }
 
+/******** main() *********************
+Main loop.
+ *****************************************/
 int main(int argc, char *argv[]){
-	 signal(SIGCHLD,SIG_IGN);
-	 int sockfd, newsockfd, portno, pid;
-	 socklen_t clilen;
-	 struct sockaddr_in serv_addr, cli_addr;
-	 int n = 0;
 
-	 memset(st_list, 0x00, 255);
-	 if (argc < 2) {
+	signal(SIGCHLD,SIG_IGN);
+	UINT sockfd, newsockfd, portno, pid;
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+	UINT n = 0;
+
+	memset(st_list, 0x00, 255);
+	if (argc < 2) {
 		fprintf(stderr,"ERROR, no port provided\n");
 		exit(1);
-	 }
-	 sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	 if (sockfd < 0) 
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
 		error("ERROR opening socket");
-	 bzero((char *) &serv_addr, sizeof(serv_addr));
-	 portno = atoi(argv[1]);
-	 serv_addr.sin_family = AF_INET;
-	 serv_addr.sin_addr.s_addr = INADDR_ANY;
-	 serv_addr.sin_port = htons(portno);
-	 if (bind(sockfd, (struct sockaddr *) &serv_addr,
-			  sizeof(serv_addr)) < 0) 
-			  error("ERROR on binding");
-	 listen(sockfd,5);
-	 clilen = sizeof(cli_addr);
-	 while (1) {
-		 newsockfd = accept(sockfd, 
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+		sizeof(serv_addr)) < 0) 
+		error("ERROR on binding");
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	while (1) {
+		newsockfd = accept(sockfd, 
 			(struct sockaddr *) &cli_addr, &clilen);
-		 st_list[st_i] = newsockfd;
-		 st_i++;
-		 printf("(Parent)size of i and list : %d\n", st_i);
-		 if (newsockfd < 0) 
+		st_list[st_i] = newsockfd;
+		st_i++;
+		printf("(Parent)size of i and list : %d\n", st_i);
+		if (newsockfd < 0)
 			error("ERROR on accept");
-		 pid = fork();
-		 if (pid < 0)
+		pid = fork();
+		if (pid < 0)
 			error("ERROR on fork");
-		 if (pid == 0)  {
-			 printf("(Parent)Client connected: Address: %s Pid: %d\n", inet_ntoa(cli_addr.sin_addr), newsockfd);
-			 close(sockfd);
-			 dostuff(newsockfd);
-			 exit(0);
+		if (pid == 0)  {
+			printf("(Parent)Client connected: Address: %s Pid: %d\n", inet_ntoa(cli_addr.sin_addr), newsockfd);
+			close(sockfd);
+			dostuff(newsockfd);
+			exit(0);
 		}
 		 //else close(newsockfd);
-	 } /* end of while */
-	 close(sockfd);
-	 return 0; /* we never get here */
+	} /* end of while */
+	close(sockfd);
+	return 0; /* we never get here */
 }
 
 /******** DOSTUFF() *********************
@@ -94,27 +113,27 @@ int main(int argc, char *argv[]){
 void dostuff (int sock){
 
 	int n, a = 0;
-	char buffer[256];
 
-	dostuff_traffic(1);
-	dostuff_traffic(3);
+	/* SEND a Logged in signal */
+	dostuff_traffic(CH_LOGGED);
 
+	/* Process Traffice loop */
 	while(1){
-		dostuff_traffic(2);
-		printf("(Child %d) st_count: %d\n",sock,st_count);
-		bzero(buffer,256);
-		n = read(sock,buffer,255);
-		if (n < 0) error("ERROR reading from socket");
-		printf("(Child %d) Here is the message: %s\n",sock,buffer);
-		if(n >= 0){
-		   //n = write(sock,"cmd$> ",6);
-			if(st_count > 1){
-				for(a = 0; a < st_count; a++){
-//				   if(st_list[a] != st_list[st_i-1]) n = write(st_list[a], buffer, 256);
-					n = write(st_list[a], buffer, 256);
+
+		bzero(c_Data, 256);	//clear buffer
+		n = read(sock, c_Data, 255);	//initiate read
+		if (n < 0) error("ERROR reading from socket"); //exit when failed
+		size_buff = strlen(c_Data);	//size of string buffer
+		printf("(Child %d) Here is the message: %s\n", sock, c_Data);
+
+		if(n > 0){
+			if(size_buff > 0){
+				for(a = 0; a <= size_buff; a++){
+//					n = write(st_list[a], buffer, 256);
 				}
 			}
 		}
+
 		if (n < 0) error("ERROR writing to socket");
 	}
 }
@@ -174,24 +193,54 @@ void dostuff_traffic (int proc){
 		n = write(sockfd, buffer, strlen(buffer));
 		if (n < 0) error("ERROR writing to socket");
 
+		/* Write & update his Client list */
+		bzero(buffer,256);
+		for(a = 0; a < st_i; a++){
+			snprintf(&buffer[a], st_i+1, "%d", st_list[a]);
+//			printf("%c ", buffer[a]);
+		}
+//		printf("\n");
+		n = write(sockfd, buffer, strlen(buffer));
+		if (n < 0) error("ERROR writing to socket");
+
 		/* Close the socket */
 		close(sockfd);
 		return;
 
 	}else if(proc == 2){
 
-		/* Write Get request first */
+		/* Write request first */
 		bzero(buffer,256);
-		sprintf(buffer, "get");
+		sprintf(buffer, "data");
+		n = write(sockfd, buffer, strlen(buffer));
+		if (n < 0) error("ERROR writing to socket");
+
+		/* Write data to server buffer */
+		bzero(buffer,256);
+//		sprintf(buffer, "data");
 		n = write(sockfd, buffer, strlen(buffer));
 		if (n < 0) error("ERROR writing to socket");
 
 		/* Then, read the incoming client count */
+//		bzero(buffer,256);
+//		n = read(sockfd,buffer,255);
+//		if (n < 0) error("ERROR reading from socket");
+//		st_count = atoi(buffer);
+//		printf("(Child) Client count is %d\n", st_count);
+
 		bzero(buffer,256);
+		memset(st_list, 0x00, 255);
 		n = read(sockfd,buffer,255);
 		if (n < 0) error("ERROR reading from socket");
-		st_count = atoi(buffer);
-		printf("(Child) Client count is %d\n", st_count);
+
+		size_buff = strlen(buffer);
+//		printf("(Child) Child has recv new Client list: ");
+		for(a = 0; a < strlen(buffer); a++){
+			st_list[a] = (int)(buffer[a] - '0');
+//			printf("%d ", st_list[a]);
+//			printf("%c ", buffer[a]);
+		}
+//		printf("\n");
 
 		/* Close the socket */
 		close(sockfd);
@@ -201,12 +250,6 @@ void dostuff_traffic (int proc){
 
 		/* Write Client list */
 		bzero(buffer,256);
-		for(a = 0; a < st_i; a++){
-			snprintf(&buffer[a], st_i+1, "%d", st_list[a]);
-			//sprintf(&buffer[a], "%d", st_list[a]);
-			printf("%c ", buffer[a]);
-		}
-		printf("\n");
 		n = write(sockfd, buffer, strlen(buffer));
 		if (n < 0) error("ERROR writing to socket");
 
@@ -216,4 +259,48 @@ void dostuff_traffic (int proc){
 
 	}
 
+}
+
+void dostuff_Relay (int proc){
+
+	int n, a = 0;
+	int sockfd, newsockfd, portno;
+	struct sockaddr_in serv_addr, cli_addr;
+
+	/* Setup a socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = RELAY_SERVER_PORT; //tcp port 50010
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_port = htons(portno);
+
+	/* connect with server's socket */
+	int res = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	
+	/* If failed we exit the function */
+	if(res == -1){
+		exit(1);
+	};
+
+	if(proc == 1){
+
+		/* Write request first */
+		bzero(buffer,256);
+		sprintf(buffer, "data");
+		n = write(sockfd, buffer, strlen(buffer));
+		if (n < 0) error("ERROR writing to socket");
+
+		/* Write data to server buffer */
+//		sprintf(buffer, "data");
+		n = write(sockfd, c_Data, strlen(c_Data));
+		if (n < 0) error("ERROR writing to socket");
+
+		/* Close the socket */
+		close(sockfd);
+		return;
+
+	}
 }
--- server/traffic.c
+++ server/traffic.c
@@ -14,17 +14,24 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
+#include "GenericTypeDefs.h"		//Generic Type Definitions
 
-static int st_list[255];
-static int st_count = 0;
-static int i = 0;
+static UINT st_list[255];			//Socket list array
+static UINT st_count = 0;
+static UINT st_i = 0;				//Socket list count
+BYTE uc_list[256];
 
-void error(const char *msg)
-{
-	perror(msg);
-	exit(1);
+/******** error() *********************
+Exit when error occurs.
+ *****************************************/
+void error(const char *msg){
+	perror(msg);		//do process error
+	exit(1);			//exit
 }
 
+/******** printarray() *********************
+Print array for debugging.
+ *****************************************/
 void printarray (int arg[], int length) {
 	int n;
 	for (n=0; n<length; ++n){
@@ -33,37 +40,40 @@ void printarray (int arg[], int length) {
 	printf("\n");
 }
 
+/******** main() *********************
+Main loop.
+ *****************************************/
 int main(int argc, char *argv[])
 {
-//     signal(SIGCHLD,SIG_IGN);
-	 int sockfd, newsockfd, portno, pid;
-	 socklen_t clilen;
-	 struct sockaddr_in serv_addr, cli_addr;
-	 int n, a = 0;
+    signal(SIGCHLD,SIG_IGN);
+	int sockfd, newsockfd, portno, pid;
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+	int n, a = 0;
 	char buffer[256];
 
-	 //memset(list, 0x00, 255);
-	 if (argc < 2) {
-		 fprintf(stderr,"ERROR, no port provided\n");
-		 exit(1);
-	 }
-	 sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	 if (sockfd < 0) 
+	memset(st_list, 0x00, 255);
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
 		error("ERROR opening socket");
-	 bzero((char *) &serv_addr, sizeof(serv_addr));
-	 portno = atoi(argv[1]);
-	 serv_addr.sin_family = AF_INET;
-	 serv_addr.sin_addr.s_addr = INADDR_ANY;
-	 serv_addr.sin_port = htons(portno);
-	 if (bind(sockfd, (struct sockaddr *) &serv_addr,
-			  sizeof(serv_addr)) < 0) 
-			  error("ERROR on binding");
-	 listen(sockfd,5);
-	 clilen = sizeof(cli_addr);
-	 while (1) {
-		 newsockfd = accept(sockfd, 
-			   (struct sockaddr *) &cli_addr, &clilen);
-		 if (newsockfd < 0)
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+		sizeof(serv_addr)) < 0) 
+		error("ERROR on binding");
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	while (1) {
+		newsockfd = accept(sockfd, 
+			(struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0)
 			 error("ERROR on accept");
 //        printf("Client connected: Address: %s Pid: %d\n", inet_ntoa(cli_addr.sin_addr), newsockfd);
 		while(1){
@@ -71,24 +81,51 @@ int main(int argc, char *argv[])
 			n = read(newsockfd,buffer,255);
 			if (n < 0) error("ERROR reading from socket");
 			if(strncmp(buffer, "logged", 6) == 0){
-				i++;
-				printf("(traffic_P) Child has updated client count to %d\n", i);
-			}else if(strncmp(buffer, "get", 3) == 0){
-				printf("(traffic_P) Child has requested a client count\n");
+
+				st_i++;
+				printf("(traffic_P) Child has updated client count to %d\n", st_i);
+
 				bzero(buffer,256);
-				sprintf(buffer, "%d\n", i);
-				n = write(newsockfd, buffer, 256);
-				if (n < 0) error("ERROR writing to socket");
-				printf("(traffic_P) Done!\n");
-			}else{
+				n = read(newsockfd,buffer,255);
+				if (n < 0) error("ERROR reading from socket");
+
 				memset(st_list, 0x00, 255);
+				bzero(uc_list,256);
 				printf("(traffic_P) Child has sent Client list: ");
 				for(a = 0; a < strlen(buffer); a++){
 					st_list[a] = (int)(buffer[a] - '0');
-					printf("%d ", st_list[a]);
+					uc_list[a] = buffer[a];
+					printf("%c ", uc_list[a]);
 				}
-				printf("\n");
+				printf("!\n");
 				printarray(st_list, strlen(buffer));
+
+			}else if(strncmp(buffer, "data", 3) == 0){
+
+				printf("(traffic_P) Child has write request on buffer\n");
+
+//				bzero(buffer,256);
+//				sprintf(buffer, "%d", st_i);
+//				n = write(newsockfd, buffer, 256);
+//				if (n < 0) error("ERROR writing to socket");
+
+				n = write(newsockfd, uc_list, 256);
+				if (n < 0) error("ERROR writing to socket");
+				printf("(traffic_P) Done!\n");
+
+			}else{
+
+//				memset(st_list, 0x00, 255);
+//				bzero(uc_list,256);
+//				printf("(traffic_P) Child has sent Client list: ");
+//				for(a = 0; a < strlen(buffer); a++){
+//					st_list[a] = (int)(buffer[a] - '0');
+//					uc_list[a] = buffer[a];
+//					printf("%c ", uc_list[a]);
+//				}
+//				printf("!\n");
+//				printarray(st_list, strlen(buffer));
+
 			}
 			if(n > 0){
 				close(newsockfd);
